0d8316438129bf36d644509964ce2bc959d60d12
Adam Congdon <cac8907@gmail.com>
2022-02-02 21:39:02 -0500

    update color

--- run_File.py
+++ run_File.py
@@ -72,7 +72,7 @@ def start_program():
 
     csv_file = pd.read_csv('outfile')
     #fig = px.scatter(csv_file, x="date", y="count", text="count")
-    fig = px.bar(csv_file, x="date", y="count", line_color='#54b948')
+    fig = px.bar(csv_file, x="date", y="count")
     #fig.update_traces(textposition="top left")
     colors = {"background": "#1d6b5b", "text": "#54b948"}
     fig.update_layout(
@@ -85,8 +85,8 @@ def start_program():
     df = pd.DataFrame(csv_file)
     df['daily_count'] = df['count'].diff()
     df.drop_duplicates(subset=None,keep='first',inplace=False)
-    fig2 = px.line(df, x="date", y="daily_count", markers=True, text="daily_count", line_color='#54b948')
-    fig2.update_traces(textposition="top left")
+    fig2 = px.line(df, x="date", y="daily_count", markers=True, text="daily_count")
+    fig2.update_traces(textposition="top left",line_color='#54b948')
     fig2.update_layout(
         plot_bgcolor=colors["background"],
         paper_bgcolor=colors["background"],
